88ac068607fa21b331a44a12118bbf71f76b7655
shaian <zshaian@users.noreply.github.com>
2025-03-29 15:49:01 +0800

    chore: add an effect to the tag input

--- components/mvp/create-snippet-form.tsx
+++ components/mvp/create-snippet-form.tsx
@@ -137,10 +137,13 @@ export default function CreateSnippetForm() {
             customClass={{
               inputTagLabelElement: 'font-[inherit]',
               inputTagContainerElement: 'font-[inherit]',
+              inputTagInputElement:
+                'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent !px-3 !py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
               inputTagListContainerElement: '!p-4 !border !border-input !gap-2',
               inputTagTagItemElement: '!bg-input/30 !py-0 border border-input',
               inputTagTagContentElement: '!text-foreground',
-              inputTagTagRemoveBtnElement: 'text-sm !text-foreground',
+              inputTagTagRemoveBtnElement:
+                'text-sm !text-foreground hover:!text-destructive trasition',
             }}
             value={snippetTags}
             onChange={setSnippetTags}
