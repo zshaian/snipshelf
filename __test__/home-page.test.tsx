60649571e8b5acdc25dba2901b84cfa07c7c2ec3
shaian <zshaian@users.noreply.github.com>
2025-03-31 18:32:01 +0800

    docs: add TODO comment

--- __test__/home-page.test.tsx
+++ __test__/home-page.test.tsx
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
+// TODO: Fix the error, where accessing jest DOM matchers is not working.
 describe('Home Component', () => {
   it('renders the title correctly', () => {
     render(<Home />);
