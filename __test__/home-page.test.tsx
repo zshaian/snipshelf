68e37fd65a2292fb4ef71d6b669b286e21e939c1
shaian <zshaian@users.noreply.github.com>
2025-03-31 15:52:08 +0800

    test: create and setup test folder

---
+++ __test__/home-page.test.tsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/page';
+
+describe('Home Component', () => {
+  it('renders the title correctly', () => {
+    render(<Home />);
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'project demo route'
+    );
+  });
+
+  it('renders all navigation links', () => {
+    render(<Home />);
+
+    const expectedLinks = [
+      { text: 'Login Page', href: '/mvp/login' },
+      { text: 'Snippets Page', href: '/mvp/snippets' },
+      { text: 'Snippets Create Page', href: '/mvp/snippets/create' },
+      { text: 'Snippets View page', href: '/mvp/snippets/view' },
+      { text: 'Snippets Profile Page', href: '/mvp/snippets/profile' },
+    ];
+
+    expectedLinks.forEach(({ text, href }) => {
+      const link = screen.getByRole('link', { name: text });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', href);
+    });
+  });
+
+  it('renders links in a list', () => {
+    render(<Home />);
+    const list = screen.getByRole('list');
+    const listItems = screen.getAllByRole('listitem');
+    expect(list).toBeInTheDocument();
+    expect(listItems).toHaveLength(5);
+  });
+});
