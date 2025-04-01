9d10faaa7b4f2c580ed9bbdffae71d8e66ecaba8
shaian <zshaian@users.noreply.github.com>
2025-04-01 14:27:21 +0800

    feat: create index file for UI components export

---
+++ components/ui/index.tsx
@@ -0,0 +1,80 @@
+export { Button } from '@/components/ui/button';
+
+export { Checkbox } from '@/components/ui/checkbox';
+
+export {
+  DropdownMenu,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+} from '@/components/ui/dropdown-menu';
+
+export {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+  useFormField,
+} from '@/components/ui/form';
+
+export {
+  Pagination,
+  PaginationContent,
+  PaginationLink,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
+} from '@/components/ui/pagination';
+
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+export { Input } from '@/components/ui/input';
+
+export { Label } from '@/components/ui/label';
+
+export { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+
+export { Separator } from '@/components/ui/separator';
+
+export { Switch } from '@/components/ui/switch';
+
+export { Textarea } from '@/components/ui/textarea';
+
+export {
+  Drawer,
+  DrawerPortal,
+  DrawerOverlay,
+  DrawerTrigger,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerDescription,
+} from '@/components/ui/drawer';
