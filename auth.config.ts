e321a978eb9108b9b79954e36dcb39cd8331e075
shaian <zshaian@users.noreply.github.com>
2025-04-02 10:42:34 +0800

    feat: install and setup next-auth js

---
+++ auth.config.ts
@@ -0,0 +1,10 @@
+import type { NextAuthConfig } from 'next-auth';
+
+import Github from 'next-auth/providers/github';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  providers: [Github],
+} satisfies NextAuthConfig;
