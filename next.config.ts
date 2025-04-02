711e09c102fb73309497c907b6312f6c731b0a6f
shaian <zshaian@users.noreply.github.com>
2025-04-02 12:11:01 +0800

    feat: add remote pattern for github images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        // remote pattern for images in github
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        search: '?v=4',
+      },
+      // TODO: implement the remote pattern for images on google
+    ],
+  },
 };
 
 export default nextConfig;
