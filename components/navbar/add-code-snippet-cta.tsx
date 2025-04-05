a1f81d962e4b3401bbc72e5e90f7f06805773a10
shaian <zshaian@users.noreply.github.com>
2025-04-05 19:18:22 +0800

    chore: hide the CTA button if we are on the create or edit snippet page

--- components/navbar/add-code-snippet-cta.tsx
+++ components/navbar/add-code-snippet-cta.tsx
@@ -2,14 +2,22 @@ import Link from 'next/link';
 import { FiPlus } from 'react-icons/fi';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
 
 export default function AddCodeSnippetCta() {
+  const pathName = usePathname();
+
+  // Check if the current path is either '/snippets/create' or '/snippets/edit', if its hide the CTA.
+  const isOnCreateOrEditSnippetPage =
+    pathName.includes('/snippets/create') ||
+    pathName.includes('/snippets/edit');
+
   return (
     <Link
       href="#"
       className={cn(
         buttonVariants({ variant: 'outline' }),
-        '!py-1 !px-1 rounded-sm lg:!py-1 lg:!px-2 h-auto flex items-center capitalize lg:rounded-sm'
+        `!py-1 !px-1 rounded-sm lg:!py-1 lg:!px-2 h-auto ${isOnCreateOrEditSnippetPage ? 'flex' : 'hidden'} items-center capitalize lg:rounded-sm`
       )}
     >
       <FiPlus />
