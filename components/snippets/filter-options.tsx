d98ab14ce9a899c01090eb6e0059fed1afd59d49
shaian <zshaian@users.noreply.github.com>
2025-04-09 20:24:10 +0800

    chore: add a dynamic search params using the filter-option component

--- components/snippets/filter-options.tsx
+++ components/snippets/filter-options.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Select,
   SelectValue,
@@ -6,29 +8,62 @@ import {
   SelectItem,
   Input,
 } from '@/components/ui';
+import programmingLanguages from '@/data/programming-languages.json';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function FilterOptions() {
-  const programmingLanguages: Array<string> = [
-    'javasript',
-    'typescript',
-    'python',
-  ];
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const handleTitleSearch = (search: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (search) {
+      params.set('title', search);
+    } else {
+      params.delete('title');
+    }
+    replace(`?${params.toString()}`);
+  };
+
+  const handleLanguageSelect = (language: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (language) {
+      params.set('language', language);
+    } else {
+      params.delete('language');
+    }
+    replace(`?${params.toString()}`);
+  };
 
   return (
     <div className="flex flex-col items-center gap-2 lg:flex-row lg:justify-between">
-      <Select>
-        <SelectTrigger className="w-full lg:w-[250px] capitalize">
+      <Select
+        onValueChange={(language) => handleLanguageSelect(language)}
+        defaultValue={searchParams.get('language')?.toString()}
+      >
+        <SelectTrigger className="w-full lg:w-[250px] uppercase">
           <SelectValue placeholder="Select a Language"></SelectValue>
         </SelectTrigger>
         <SelectContent className="capitalize">
           {programmingLanguages.map((language) => (
-            <SelectItem key={language} value={language}>
-              {language}
+            <SelectItem
+              key={language.programmingLanguageName}
+              value={language.programmingLanguageName}
+              className="uppercase"
+            >
+              {language.programmingLanguageName}
             </SelectItem>
           ))}
         </SelectContent>
       </Select>
-      <Input placeholder="Search Snippets..." className="lg:w-[250px]" />
+
+      <Input
+        placeholder="Search Snippets..."
+        className="lg:w-[250px]"
+        onChange={(event) => handleTitleSearch(event.target.value)}
+        defaultValue={searchParams.get('title')?.toString()}
+      />
     </div>
   );
 }
