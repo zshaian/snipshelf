1273568b402d7dfa344e30be95547cec6ca4127b
shaian <zshaian@users.noreply.github.com>
2025-04-10 18:45:35 +0800

    feat: add the pagination component

---
+++ components/pagination.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui';
+import { generatePagination } from '@/lib/generate-pagination';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+export default function SnippetsPagination({
+  totalPages,
+}: {
+  totalPages: number;
+}) {
+  const pathName = usePathname();
+  const searchParam = useSearchParams();
+  const currentPage = Number(searchParam.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
+
+  const createPageURL = (pageNumber: string | number) => {
+    const params = new URLSearchParams(searchParam);
+    params.set('page', pageNumber.toString());
+    return `${pathName}/?${params.toString()}`;
+  };
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            hidden={currentPage <= 1}
+            href={createPageURL(currentPage - 1)}
+          />
+        </PaginationItem>
+
+        {allPages.map((page) => {
+          // if the pagination content is an elipsis
+          if (page === '...') {
+            return (
+              <PaginationItem key={page}>
+                <PaginationEllipsis />
+              </PaginationItem>
+            );
+          }
+
+          return (
+            <PaginationItem key={page}>
+              <PaginationLink
+                href={createPageURL(page)}
+                isActive={page === currentPage}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          );
+        })}
+
+        <PaginationItem>
+          <PaginationNext
+            hidden={currentPage >= totalPages}
+            href={createPageURL(currentPage + 1)}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
