20f359d12fb3f5b0312777e0f781c2619a40d671
AdvaitMadhekarOfficial <57474725+AdvaitMadhekarOfficial@users.noreply.github.com>
2025-04-10 22:14:04 -0700

    Update settings.py

--- snipshelf_service/snipshelf_service/settings.py
+++ snipshelf_service/snipshelf_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)eyfkky$$+8tzh^2=$g4^j1q6frxoq1#&mq7_#ow@lz9+4-pn#'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
