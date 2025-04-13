51bf940646dac75b5fe1f8adb60fc903d2604590
shaian <zshaian@users.noreply.github.com>
2025-04-13 14:21:11 +0800

    chore: add the initial file for the landing page

---
+++ app/landing/page.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    // import and use the necessary components in here
+    //  navbar, hero section, features of the landing page
+    // put it inside of the main element
+    <main></main>
+  );
+}
