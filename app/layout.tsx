db8ae9f75c4a4bf787be2983c705781fea20c873
shaian <zshaian@users.noreply.github.com>
2025-04-14 14:37:42 +0800

    chore: separate the stylesheet in the app folder for organization

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from '@/components/theme-provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '@/styles/globals.css';
 
 const inter = Inter({
   variable: '--font-inter',
