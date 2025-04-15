072f297bd8360a0da822a08a71034bd82bfb40b7
shaian <zshaian@users.noreply.github.com>
2025-04-15 15:34:37 +0800

    chore: add a loading skeleton to the code editor

--- components/snippets/snippet-editor.tsx
+++ components/snippets/snippet-editor.tsx
@@ -2,6 +2,7 @@
 
 import { Editor } from '@monaco-editor/react';
 import { useTheme } from 'next-themes';
+import { Skeleton } from '@/components/ui';
 
 type SnippetEditorProps = {
   programmingLanguageName: string;
@@ -19,6 +20,7 @@ export default function SnippetEditor({
       theme={resolvedTheme === 'dark' ? 'vs-dark' : 'light'}
       language={programmingLanguageName.toLowerCase()}
       options={{ fontSize: 16 }}
+      loading={<Skeleton className="h-full w-full rounded-none" />}
       {...props}
     />
   );
