f80242e8d1558c43fce08529acdc694de1329665
Tarak Nath Paul <passenger.code016@gmail.com>
2025-04-25 11:07:26 +0530

    refactor(landing): moved hero markup from page.tsx to hero.tsx and integrated component

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -1,3 +1,27 @@
+import { Button } from '../ui';
+
 export default function Hero() {
-  return <section>{/* put the title and subheading here */}</section>;
+  return (
+    <section>
+      {/* Hero Section Container */}
+      <div className="max-w-full min-h-screen flex flex-col justify-center items-center gap-8 bg-amber-600">
+        {/* Primary Heading Container */}
+        <h1 className="flex flex-col justify-center items-center text-7xl font-medium">
+          <span>The Snippet Tool</span>
+          <span>For Developers</span>
+        </h1>
+        {/* Secondary Heading Container */}
+        <h2 className="max-w-[476px] bg-amber-700 text-center text-4xl font-regular">
+          <span>Save Share, and Organize Code Snippets.</span>
+        </h2>
+        {/* Button Container */}
+        <div className="max-w-full flex justify-center items-center gap-4">
+          <Button className="capitalize">Create code snippet</Button>
+          <Button variant="secondary" className="capitalize">
+            Browse code snippet
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 }
