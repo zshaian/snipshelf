9ff099c5840e7a3d2ad02c5996482bfcbb0023a6
Tarak Nath Paul <passenger.code016@gmail.com>
2025-04-25 16:02:57 +0530

    refactor(features): improved responsiveness of the Feature Card layout

--- components/landing/features.tsx
+++ components/landing/features.tsx
@@ -8,21 +8,21 @@ export default function Features() {
     <section>
       <div className="flex justify-center items-center max-w-screen h-full md:h-96 lg:h-64">
         <ul className="flex md:flex-row flex-col justify-center items-center gap-4 w-full lg:w-[80%] h-full">
-          <li className="w-full flex items-center justify-center">
+          <li className="w-full flex items-center justify-center h-full">
             <FeatureCard
               title="Search for your & other's code snippets"
               description="Find the exact code snippet you need in seconds using search and language based filters"
               Icon={Search}
             />
           </li>
-          <li className="w-full flex items-center justify-center">
+          <li className="w-full flex items-center justify-center h-full">
             <FeatureCard
               title="Share your code snippets with others"
               description="Share your code snippets with a link, perfect for feedback or just showing off your work"
               Icon={Share2}
             />
           </li>
-          <li className="w-full flex items-center justify-center">
+          <li className="w-full flex items-center justify-center h-full">
             <FeatureCard
               title="Create your own code snippets"
               description="Add and manage your code snippets effortlessly, with support for multiple languages"
