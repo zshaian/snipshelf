0467ce1b7919928c41554e6f68466a1398f859f4
shaian <zshaian@users.noreply.github.com>
2025-05-05 04:01:55 +0800

    feat: update the main page o the app to load the dynamic snippets

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,51 @@
-import Link from 'next/link';
+import Navbar from '@/components/navbar/navbar';
+import SnippetsPagination from '@/components/pagination';
+import FilterOptionsSkeleton from '@/components/skeleton/filter-snippets';
+import PaginationSkeleton from '@/components/skeleton/pagination';
+import SnippetListSkeleton from '@/components/skeleton/snippet-list';
+import FilterOptions from '@/components/snippets/filter-options';
+import SnippetList from '@/components/snippets/snippet-list';
+import { Suspense } from 'react';
+import { getSnippetList, getPagination } from '@/services';
+import SignOutButton from '@/components/sign-out-button';
 
-export default function Home() {
+export default async function MainPage({
+  searchParams,
+}: {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ title?: string; language?: string; page?: string }>;
+}) {
+  const { title = '', language = '', page = '1' } = await searchParams;
+
+  const snippetList = getSnippetList({
+    title,
+    language,
+    page: Number(page),
+  });
+
+  const pagination = getPagination({
+    title,
+    language,
+  });
+
   return (
-    <main className="p-8 flex flex-col gap-4 items-center">
-      <h1 className="uppercase text-3xl font-black">project demo route</h1>
-      <ul className="text-blue-400 flex flex-col gap-2 items-start text-xl">
-        <li>
-          <Link href="/mvp/login" className="hover:underline">
-            Login Page
-          </Link>
-        </li>
-        <li>
-          <Link href="/mvp/snippets" className="hover:underline">
-            Snippets Page
-          </Link>
-        </li>
-        <li>
-          <Link href="/mvp/snippets/create" className="hover:underline">
-            Snippets Create Page
-          </Link>
-        </li>
-        <li>
-          <Link href="/mvp/snippets/view" className="hover:underline">
-            Snippets View page
-          </Link>
-        </li>
-        <li>
-          <Link href="/mvp/snippets/profile" className="hover:underline">
-            Snippets Profile Page
-          </Link>
-        </li>
-      </ul>
-    </main>
+    <>
+      <Navbar />
+      <main className="p-8 flex flex-col gap-4">
+        <Suspense fallback={<FilterOptionsSkeleton />}>
+          <FilterOptions />
+        </Suspense>
+
+        <Suspense fallback={<SnippetListSkeleton />}>
+          <SnippetList snippetListRequest={snippetList} />
+        </Suspense>
+
+        <Suspense fallback={<PaginationSkeleton />}>
+          <SnippetsPagination totalPagesRequest={pagination} />
+        </Suspense>
+
+        <SignOutButton />
+      </main>
+    </>
   );
 }
