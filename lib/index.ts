cf2219ad7de1a3c3cda639b8ae67f8e590ffff53
shaian <zshaian@users.noreply.github.com>
2025-05-09 02:28:23 +0800

    chore: correct the typo in the file name

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 export { copyTextToClipBoard } from '@/lib/copy-text';
 export { generatePagination } from '@/lib/generate-pagination';
 export { getLanguageColor } from '@/lib/get-language-color';
-export { formatCreationDate } from '@/lib/fomat-creatoin-date';
+export { formatCreationDate } from '@/lib/fomat-creation-date';
 export { cn } from '@/lib/utils';
