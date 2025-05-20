3a55a8878eeb6b0624afbf2f12513bd3321404dd
shaian <zshaian@users.noreply.github.com>
2025-05-21 02:09:01 +0800

    feat: generate dynamic metadata for title and description of the snippets

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,14 +1,25 @@
 import Navbar from '@/components/navbar/navbar';
 import SnippetViewSkeleton from '@/components/skeleton/snippet-view';
 import SnippetView from '@/components/snippets/snippet-view';
-import { getSnippetInfo } from '@/services';
+import { getSnippetInfo, getSnippetInfoMetadata } from '@/services';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
-
-export default async function SnippetViewPage({
-  params,
-}: {
+type SnippetProps = {
   params: Promise<{ id: string }>;
-}) {
+};
+
+export async function generateMetadata({
+  params,
+}: SnippetProps): Promise<Metadata> {
+  const { id } = await params;
+  const snippetInfo = await getSnippetInfoMetadata(id);
+  return {
+    title: `${snippetInfo.title || 'No Title'} | Snipshelf`,
+    description: snippetInfo.description || 'No Description',
+  };
+}
+
+export default async function SnippetViewPage({ params }: SnippetProps) {
   const { id } = await params;
   const snippetInfo = getSnippetInfo(id);
 
